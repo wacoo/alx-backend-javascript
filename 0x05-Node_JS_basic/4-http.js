03e99de0aef708ea256fa24a49cc0cf176e41866
wac <wabaham9@gmail.com>
2023-08-22 22:02:31 +0300

    task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+/* write an  http server */
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = '1234';
+
+const app = http.createServer((request, result) => {
+  result.setHeader('Content-Type', 'text/plain');
+  result.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {
+});
+
+module.exports = app;
